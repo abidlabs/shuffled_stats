f4eb5096b1b56ae880f23a8c4853da527e902609
Abubakar Abid <Abubakar Abid>
2017-04-15 22:38:42 -0700

    Fixed divide by zero bugs

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
 from distutils.core import setup
 
 setup(
-    name = 'shuffled-stats',
-    version = '1.0.0',
+    name = 'shuffled_stats',
+    version = '1.0.2',
     description = 'Python library for performing inference on datasets with shuffled labels',
     author = 'Abubakar Abid',
     author_email = 'a12d@stanford.edu',
     url = 'https://github.com/abidlabs/shuffled-stats', 
+    download_url = 'https://github.com/abidlabs/shuffled-stats/archive/1.0.0.tar.gz',
     py_modules=['shuffled-stats'],
+    keywords = ['statistics', 'shuffled', 'permutation','regression'],
     install_requires=[
         'numpy',
         'scipy',
     ],
-    entry_points='''
-        [console_scripts]
-        zaiste=zaiste:cli
-    ''',
 )
